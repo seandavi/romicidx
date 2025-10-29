1c334cdecaba3fab3d73b0a115b91776de9ef1fb
Sean Davis <seandavi@gmail.com>
2025-10-29 17:31:56 -0400

    Refactor duckdb file location getter

--- R/duckdb_connection.R
+++ R/duckdb_connection.R
@@ -1,6 +1,22 @@
 .duckdb_file = "https://store.cancerdatasci.org/omicidx/omicidx.duckdb"
 
 
+#' Get the location of the OmicIDX DuckDB database file.
+#'
+#' This function returns the URL of the OmicIDX DuckDB database file.
+#' You can use this URL to access the database directly or for other purposes.
+#'
+#' @return A string representing the URL of the OmicIDX DuckDB database file.
+#'
+#' @examples
+#' url <- get_omicidx_duckdb_location()
+#' print(url)
+#'
+#' @export
+get_omicidx_duckdb_location <- function() {
+  return(.duckdb_file)
+}
+
 #' Establish a read-only connection to the OmicIDX DuckDB database.
 #'
 #' The omicidx DuckDB database is hosted remotely and accessed via the httpfs extension.
@@ -21,8 +37,9 @@
 #' @export
 omicidx_duckdb_connection <- function() {
   con <- DBI::dbConnect(duckdb::duckdb())
+  duckdb_file <- get_omicidx_duckdb_location()
   DBI::dbExecute(con, "install 'httpfs'; LOAD 'httpfs'")
-  DBI::dbExecute(con, paste0("ATTACH DATABASE '", .duckdb_file, "' AS omicidx"))
+  DBI::dbExecute(con, paste0("ATTACH DATABASE '", duckdb_file, "' AS omicidx"))
   DBI::dbExecute(con, "use omicidx")
   return(con)
 }
